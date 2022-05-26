a5a7fb99093ecd0fb394c9726a9310e71d071386
ThangTiot <nguyendatthanguet@gmail.com>
2022-05-26 23:06:56 +0700

    update

--- CASE_STUDY_MODULE1.js
+++ CASE_STUDY_MODULE1.js
@@ -63,11 +63,6 @@ function add(){
     let sl = document.getElementById("sl").value
     arr[arr.length] = new Product(img,id,name,price,sl)
     creatTable()
-    document.getElementById("img").value = ""
-    document.getElementById("ID").value = ""
-    document.getElementById("name").value = ""
-    document.getElementById("price").value = ""
-    document.getElementById("sl").value = ""
 }
 function creatTable(){
     let txt = ""
@@ -77,6 +72,11 @@ function creatTable(){
             "</td><td><button onclick='editProduct(" + i + ")'>Edit</button><button onclick='deleteProduct(" + i + ")'>Delete</button></td></tr>"
     }
     document.getElementById("listBody").innerHTML = txt
+    document.getElementById("img").value = ""
+    document.getElementById("ID").value = ""
+    document.getElementById("name").value = ""
+    document.getElementById("price").value = ""
+    document.getElementById("sl").value = ""
 }
 // arr[arr.length] = "<tr><td><img src="link" width="100" height="60"></td><td>id</td><td>name</td><td>price</td><td>sl</td><td><button onclick="edit(arr.length)">Edit</button><button onclick="delete(arr.length)">Delete</button></td></tr>"
 
@@ -86,5 +86,28 @@ function deleteProduct(i){
 }
 
 function editProduct(i){
+    document.getElementById("img").value = arr[i].getImg()
+    document.getElementById("ID").value = arr[i].getId()
+    document.getElementById("name").value = arr[i].getName()
+    document.getElementById("price").value = arr[i].getPrice()
+    document.getElementById("sl").value = arr[i].getSL()
+    document.getElementById("upDate").style.display = ""
+    document.getElementById("add").style.display = "none"
+    document.getElementById("upDate").onclick = function (){upDate(i)}
+}
 
+function upDate(i){
+    let img = document.getElementById("img").value
+    let id = document.getElementById("ID").value
+    let name = document.getElementById("name").value
+    let price = document.getElementById("price").value
+    let sl = document.getElementById("sl").value
+    arr[i].setImg(img)
+    arr[i].setId(id)
+    arr[i].setName(name)
+    arr[i].setPrice(price)
+    arr[i].setSL(sl)
+    creatTable()
+    document.getElementById("upDate").style.display = "none"
+    document.getElementById("add").style.display = ""
 }
